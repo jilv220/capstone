9ca2a99971cbeabaccd9c18a332f918dc14f2c45
jilv220 <hkfhfkcoc@protonmail.com>
2024-05-31 13:11:16 -0700

    feat: finish pwa support

--- capstone-frontend/vite.config.ts
+++ capstone-frontend/vite.config.ts
@@ -12,8 +12,31 @@ export default defineConfig({
       srcDir: "src/service-worker",
       filename: "sw.ts",
       strategies: "injectManifest",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
-        "theme_color": "#000000",
+        name: "Idk",
+        short_name: "Idk",
+        description: "A mood tracking app",
+        theme_color: "#ffffff",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
       /** Won't work on Firefox unfortunately*/
       devOptions: {
