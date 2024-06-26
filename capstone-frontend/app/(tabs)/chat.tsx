9521647fdb5076d42783bc6b4cec1764fa29cfcd
ivy <j.song1@student.fdu.edu>
2024-06-26 14:57:42 -0700

    add chat frame component

--- capstone-frontend/app/(tabs)/chat.tsx
+++ capstone-frontend/app/(tabs)/chat.tsx
@@ -1,11 +1,37 @@
-import { ScrollView, Text, YStack } from 'tamagui';
-
+import { useCallback, useEffect, useState } from 'react';
+import { ScrollView, Text, YStack, useTheme } from 'tamagui';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
+import emojiUtils from 'emoji-utils';
+import { Platform } from 'react-native';
 export default function ChatScreen() {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'hello',
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'Chatty',
+          avatar: '@/assets/images/avatar.png',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages: IMessage[] = []) => {
+    setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
+  }, []);
+
   return (
-    <ScrollView px={'$4'} py={'$7'}>
-      <YStack>
-        <Text>Chat Screen</Text>
-      </YStack>
-    </ScrollView>
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
   );
 }
