d8c7d6751e1a96e64b31b25c7d5194d21a7bf264
Lyu Ji <hkfhfkcoc@protonmail.com>
2024-07-08 16:52:21 -0700

    feat: implement article fts with meilisearch

--- capstone-backend/src/interfaces/base.ts
+++ capstone-backend/src/interfaces/base.ts
@@ -1,1 +1,2 @@
 export type ValidChatMessageRole = 'system' | 'user' | 'assistant';
+export const RESOURCE_ARTICLE_INDEX = 'resource_article';
--- capstone-backend/src/routes/v1/resource/article/search.ts
+++ capstone-backend/src/routes/v1/resource/article/search.ts
@@ -1,9 +1,29 @@
+import { meili } from '@/app.ts';
+import { RESOURCE_ARTICLE_INDEX } from '@/interfaces/base.ts';
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import { z } from 'zod';
 
 const search = new Hono().basePath('/search');
 
-search.get('/', (c) => {
-  return c.text('this is a search endpoint');
-});
+search.get(
+  '/',
+  zValidator(
+    'query',
+    z.object({
+      q: z.string().max(60).default(''),
+      limit: z.coerce.number().max(40).default(20),
+      offset: z.coerce.number().max(40).default(0),
+    })
+  ),
+  async (c) => {
+    const { q, limit, offset } = c.req.valid('query');
+    const res = await meili.index(RESOURCE_ARTICLE_INDEX).search(q, {
+      offset,
+      limit,
+    });
+    return c.json(res);
+  }
+);
 
 export default search;
