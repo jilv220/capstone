dce3b978988af8111ed9808696f51f9e124bfa9a
Lyu Ji <hkfhfkcoc@protonmail.com>
2024-07-21 17:42:36 -0700

    minor fix

--- capstone-frontend/hooks/usePrefetchToplevelScreens.ts
+++ capstone-frontend/hooks/usePrefetchToplevelScreens.ts
@@ -1,39 +1,46 @@
+import { useEffect } from "react";
 import { getAllMessages, getConversations } from "@/actions/chat";
 import { getMoodChart, getMoodCount } from "@/actions/moodLog";
 import { Conversation } from "@/interfaces/chat";
-import { useQueries, useQuery } from "@tanstack/react-query";
+import { useQueries, useQueryClient } from "@tanstack/react-query";
 
 export function usePrefetchToplevelScreens() {
-  const { data: conversations } = useQuery({
-    queryKey: ['conversation'],
-    queryFn: getConversations,
-  });
+  const queryClient = useQueryClient();
 
-  const toSorted = (conversations: Conversation[] | undefined) =>
-    conversations ? conversations.sort((a, b) => (a.updated_at < b.updated_at ? 1 : -1)) : [];
+  useEffect(() => {
+    async function prefetchData() {
+      const conversations = await queryClient.ensureQueryData({
+        queryKey: ['conversation'],
+        queryFn: getConversations,
+      });
 
-  const prevYear = [...Array(12)].map((_, i) => i);
+      const prevYear = [...Array(12)].map((_, i) => i);
+      await Promise.all([
+        ...prevYear.map((prevNMonth) => queryClient.prefetchQuery({
+          queryKey: ['mood-log', 'mood-avg', prevNMonth],
+          queryFn: () => getMoodChart(prevNMonth),
+        })),
+        ...prevYear.map((prevNMonth) => queryClient.prefetchQuery({
+          queryKey: ['mood-log', 'mood-count', prevNMonth],
+          queryFn: () => getMoodCount(prevNMonth),
+        })),
+      ]);
 
-  useQueries({
-    queries: prevYear.map((prevNMonth) => ({
-      queryKey: ['mood-log', 'mood-avg', prevNMonth],
-      queryFn: () => getMoodChart(prevNMonth)
-    })) 
-  }); 
+      const toSorted = (conversations: Conversation[] | undefined) =>
+        conversations ? conversations.sort((a, b) => (a.updated_at < b.updated_at ? 1 : -1)) : [];
+      await Promise.all(
+        toSorted(conversations).slice(0, 4).map((c) =>
+          queryClient.prefetchQuery({
+            queryKey: ['conversation', c.id],
+            queryFn: () => getAllMessages(c.id),
+          })
+        )
+      );
+    }
 
-  useQueries({
-    queries: prevYear.map((prevNMonth) => ({
-      queryKey: ['mood-log', 'mood-count', prevNMonth],
-      queryFn: () => getMoodCount(prevNMonth)
-    }))
-  }) 
-
-  useQueries({
-    queries: toSorted(conversations)
-      .slice(0, 4)
-      .map((c) => ({
-        queryKey: ['conversation', c.id],
-        queryFn: () => getAllMessages(c.id),
-      })),
-  });
+    prefetchData()
+      .catch((e) => {
+        console.warn(`Prefetch failed. Reason: ${e}`)
+      });
+  }, [queryClient]);
 }
